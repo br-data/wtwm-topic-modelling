4eae89cb3c6acefcac76bc491c14f8a6bfb0a96f
JaimeAvalos <jaimeavalos@criptext.com>
2022-10-03 23:41:07 +0200

    improve the function load_data so it loads data into ida BigQuery correctly

--- endpointTasks/load_data.py
+++ endpointTasks/load_data.py
@@ -4,17 +4,18 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.cloud.bigquery import LoadJobConfig
 from google.cloud.bigquery import SourceFormat
+from os import path
 
 class LoadData:
     def __init__(self):
         self.credentials = service_account.Credentials.from_service_account_file(
-            './creds/journalismai-credentials.json')
+            '.credentials/journalismai-credentials.json')
         self.project_id = 'journalismai-362410'
         self.dataset_id = 'wtwm'
         self.client = bigquery.Client(credentials=self.credentials, project=self.project_id)
 
-    def load_data(self, json_file: str, table_id: str):
-        '''loads data into BigQuery'''
+    def load_data(self, file_name: str, table_id: str):
+        '''loads comments data into the BigQuery table specified by table_id'''
 
         # create a dataset reference
         dataset_ref = self.client.dataset(self.dataset_id)
@@ -28,8 +29,6 @@ class LoadData:
         load_job_config.autodetect = True
 
         # load the data
-        with open(json_file, 'rb') as source_file:
+        with open(path.abspath('.') + '/data/' + file_name, 'rb') as source_file:
             job = self.client.load_table_from_file(source_file, table_ref, job_config=load_job_config)
             job.result()
-
-        # wait for the job to complete
