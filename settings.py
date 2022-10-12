650a621fcbea3de388c96875c2cff5599caabd21
Philipp Gawlik <philipp.gawlik@posteo.de>
2022-10-12 11:05:49 +0200

    :construction_worker: Updated model path

--- settings.py
+++ settings.py
@@ -1,10 +1,3 @@
 import os
 
-try:
-    _MODEL_PATH = os.environ["MODEL_PATH"]
-except (KeyError, TypeError):
-    raise ValueError("Missing environment variable: 'MODEL_PATH'")
-
-# if deployed on server
-BUCKET_PATH = os.environ.get("VOLUME_PATH", "")
-MODEL_PATH = f"{BUCKET_PATH}{_MODEL_PATH}"
+_MODEL_PATH = os.environ.get("MODEL_PATH", "model/detect_mentions/")
