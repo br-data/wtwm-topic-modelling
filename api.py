249234c49424bb0450b3c661e81e30aa256f5b52
Philipp Gawlik <philipp.gawlik@posteo.de>
2022-10-12 14:39:12 +0200

    :zap: Added endpoint to reload model

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 import uvicorn
 
 import spacy
 
-from src.api.response_models import ExtractorResponse
+from src.api.response_models import ExtractorResponse, ModelReloadResponse, ErrorCode
 from src.api.request_models import ExtractorRequestBody
 from src.extract import extract_mentions_from_text
 from settings import MODEL_PATH
@@ -51,6 +51,19 @@ async def find_mentions(body: ExtractorRequestBody) -> ExtractorResponse:
         msg = "Found no mentions."
 
     return ExtractorResponse(status="ok", msg=msg, result=result)
+
+
+
+@APP.get("/v1/reload_model", response_model=ModelReloadResponse)
+def reload_model() -> None:
+    """Reload a model from the bucket into this running API."""
+    try:
+        SPACY_MODEL = spacy.load(MODEL_PATH)
+    except OSError as exc:
+        msg = f"Couldn't find the model at: '{MODEL_PATH}' because '{exc}'"
+        raise HTTPException(status_code=ErrorCode.MODEL_NOT_FOUND.value, detail=msg)
+    else:
+        return ModelReloadResponse(status="ok", msg=f"Successfully reloaded model '{MODEL_PATH}'")
 
 
 if __name__ == "__main__":
--- src/api/response_models.py
+++ src/api/response_models.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel
 from src.models import ExtractorResult
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    MODEL_NOT_FOUND = 404
 
 
 class ExtractorResponse(BaseModel):
     status: str
     msg: str
     result: list[ExtractorResult]
+
+
+class ModelReloadResponse(BaseModel):
+    status: str
+    msg: str
