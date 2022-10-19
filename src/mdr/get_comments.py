511292b39001b46acfc94a2d9a17057505d54ac6
Philipp Gawlik <philipp.gawlik@posteo.de>
2022-10-19 13:50:16 +0200

    :construction: Added pagination

--- src/mdr/get_comments.py
+++ src/mdr/get_comments.py
@@ -13,19 +13,36 @@ class MDRCommentGetter(BaseModel):
     token: str = MDR_COMMENT_ENDPOINT_TOKEN
 
     def get_comments(
-        self, from_: datetime, to: datetime, size: int = 20, page: int = 1
+        self,
+        from_: datetime,
+        to: datetime,
+        size: int = 20,
+        start_page: int = 1,
+        max_pages: int = 20,
+        verbose: bool = True
     ) -> list[dict[str, Union[int, list[dict[str, Union[str, int]]]]]]:
         """Get commens for a specified timeframe.
 
         :param from_: begin of timeframe
         :param to: end of timeframe
         :param size: max number of return comments
-        :param page: page number
+        :param start_page: start page for result iteration
+        :param max_pages: max number of pages to iterate through
         """
-        query = self._get_filter(from_, to, size, page)
+        items = []
+        query = self._get_filter(from_, to, size, start_page)
         headers = {"Authorization": f"Bearer {MDR_COMMENT_ENDPOINT_TOKEN}"}
         response = request(self.url, body=query, headers=headers)
-        return response["items"]
+        response_items = response["items"]
+        while response_items and query["page"] < max_pages:
+            if verbose:
+                print(f"Got {len(response_items)} from page {query['page']}")
+
+            items.extend(response["items"])
+            query["page"] += 1
+            response = request(self.url, body=query, headers=headers)
+
+        return items
 
     def _get_filter(
         self, from_: datetime, to: datetime, size: int = 20, page: int = 1
