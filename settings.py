063b3692e8dfe52c0daaf7a69567afd655f726e4
Philipp Gawlik <philipp.gawlik@posteo.de>
2022-10-19 14:36:29 +0200

    Revert "adds default settings for local development"
    This reverts commit ed225c4d38564f47d9771f5fcf1657d6f913dd8b.

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 MODEL_PATH = os.environ.get("MODEL_PATH", "model/detect_mentions/")
 BACKUP_PATH = os.environ.get("BACKUP_PATH", "model/backup/")
-MDR_COMMENT_ENDPOINT_TOKEN = os.environ.get("MDR_COMMENT_ENDPOINT_TOKEN", ".credentials/coral-talk-token.json")
-MDR_COMMENT_ENDPOINT = os.environ.get("MDR_COMMENT_ENDPOINT", "./credentials/coral-talk-endpoint.json")
+MDR_COMMENT_ENDPOINT_TOKEN = os.environ["MDR_COMMENT_ENDPOINT_TOKEN"]
+MDR_COMMENT_ENDPOINT = os.environ["MDR_COMMENT_ENDPOINT"]
 BIGQUERY_PROJECT_ID = "journalismai-362410"
 BIGQUERY_CREDENTIAL_PATH = os.environ.get(
     "IDA_BIGQUERY_CREDENTIAL_PATH", "data/journalismai-credentials.json"
