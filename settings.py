ab730c95bf80f540a0be1443520d1964dc6c44fe
Philipp Gawlik <philipp.gawlik@posteo.de>
2022-11-08 16:45:26 +0100

    :wrench: Added bucket paths

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 import os
 
+# file path
 MODEL_PATH = os.environ.get("MODEL_PATH", "model/detect_mentions/")
 BACKUP_PATH = os.environ.get("BACKUP_PATH", "model/backup/")
+
+# recogniser source data
+BASELINE_SOURCE = os.environ.get("BASELINE_SOURCE_FILE", "model/baseline_regex_collection.txt")
+PATTERN_RECOGNISER_SOURCE = os.environ.get("TRIE_PATTERN_SOURCE_FILE", "model/trie_patterns.txt")
 MDR_COMMENT_ENDPOINT_TOKEN = os.environ["MDR_COMMENT_ENDPOINT_TOKEN"]
 MDR_COMMENT_ENDPOINT = os.environ["MDR_COMMENT_ENDPOINT"]
 BR_COMMENT_ENDPOINT_TOKEN = os.environ["BR_COMMENT_ENDPOINT_TOKEN"]
@@ -22,10 +27,6 @@ POSTGRES_IP = os.environ["DATABASE_ADDRESS"]
 POSTGRES_USER = os.environ["DATABASE_USER"]
 POSTGRES_PASS = os.environ["DATABASE_PASSWORD"]
 POSTGRES_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_IP}"
-
-# recogniser settings
-BASELINE_SOURCE = "model/baseline_regex_collection.txt"
-PATTERN_RECOGNISER_SOURCE = "model/trie_patterns.txt"
 
 # team settings
 MAX_NUMBER_PUBLISH = 5
